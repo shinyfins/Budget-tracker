07c69af590854e556a4edbe563e1db8889117dca
Your Name <mcpgco@gmail.com>
2022-06-26 21:19:28 -0700

    idb finished

--- public/js/idb.js
+++ public/js/idb.js
@@ -20,3 +20,54 @@ request.onerror = function(event) {
     // log errors
     console.log(event.target.errorCode);
 };
+
+function saveRecord(record) {
+
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const  budgetObjectStore = transaction.objectStore('new_transaction');
+
+    budgetObjectStore.add(record);
+}
+
+function uploadTransaction() {
+
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+                    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+                    budgetObjectStore.clear();
+
+                    alert('Transaction history saved');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction);
